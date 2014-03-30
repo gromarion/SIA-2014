d8000ade59a6a225bbc6143320da374b3e134268
German Romarion <grromarion@hotmail.com>
2014-03-30 20:02:22 -0300

    Major code refactor
    The problem wsa not properly engaged, and we needed to throw most of our code to the
    trash and think again all the models and the structures and how the rules are applied
    in out specific problem.

--- SIA-TP1/CalcuDoku/src/main/java/sia/calcudoku/Heuristic.java
+++
@@ -1,5 +0,0 @@
-package sia.calcudoku;
-
-public enum Heuristic {
-	BLOCKS, ROWS_AND_COLUMNS, BOTH
-}
--- SIA-TP1/CalcuDoku/src/main/java/sia/gps/exception/NotAppliableException.java
+++
@@ -1,7 +0,0 @@
-package sia.gps.exception;
-
-public class NotAppliableException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-}
